7760479c8ed2dfa8ceca769f03debfbf9d40cb88
JeffreyManuel <jeffreymanuel9129@gmail.com>
2023-03-10 15:38:40 +0530

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -4,10 +4,13 @@
 sudo apt-get remove --purge gdm snapd thunderbird rhythmbox totem gnome-mahjongg gnome-mines gnome-sudoku aisleriot gnome-mahjongg gnome-mines gnome-sudoku cheese shotwell gnome-orca simple-scan gnome-calculator gnome-characters gnome-logs gnome-system-monitor gnome-disk-utility gnome-font-viewer gnome-screenshot gnome-terminal transmission-gtk gnome-todo gnome-tweaks gnome-weather gnome-shell-extension-ubuntu-dock gnome-shell-extension-appindicator gnome-shell-extension-desktop-icons -y
 
 # Install KDE and SDDM
-sudo apt-get install nala kde-plasma-desktop sddm -y
+sudo apt-get install kde-plasma-desktop sddm -y
 
 # Set SDDM as default display manager
 sudo systemctl enable sddm
 sudo dpkg-reconfigure sddm
+
+# Reboot the system after 10 seconds
+echo "Rebooting the system in 10 seconds..."
 sleep 10
 sudo reboot
