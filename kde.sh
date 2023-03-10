12fcde2d1e2469b5ef49cd1f44a9d9a347ec01d9
JeffreyManuel <jeffreymanuel9129@gmail.com>
2023-03-10 15:39:31 +0530

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -6,6 +6,13 @@ sudo apt-get remove --purge gdm snapd thunderbird rhythmbox totem gnome-mahjongg
 # Install KDE and SDDM
 sudo apt-get install kde-plasma-desktop sddm -y
 
+#Install Brave Browser
+sudo apt install curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
 # Set SDDM as default display manager
 sudo systemctl enable sddm
 sudo dpkg-reconfigure sddm
